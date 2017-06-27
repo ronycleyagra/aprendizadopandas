6cd97111dd1588bea8c163eb81305bca746d0b1a
Ronycley <ronycley@gmail.com>
2017-06-27 12:39:06 -0300

    Update Exemplo5j.py

--- Exemplo5j.py
+++ Exemplo5j.py
@@ -1,31 +1,3 @@
-
-# coding: utf-8
-
-# In[29]:
-
-get_ipython().magic('matplotlib inline')
-import os
-import sys
-import pandas as pd
-import pyodbc
-import pandas.io.sql as psql
-import matplotlib.pyplot as plt
-
-cnxn = pyodbc.connect('DRIVER={Sql Server};SERVER=10.16.4.39;DATABASE=***;UID=***;PWD=***') 
-cursor = cnxn.cursor()
-sql = ("""select distinct(codrealizacao) as qtdrealizacoes, mesanocompetencia from historicorealizacoescliente""")
-
-df = psql.read_sql(sql, cnxn)
-cnxn.close()
-
-#df[['mesanocompetencia','qtdrealizacoes']]
-
-qtd = df['mesanocompetencia'].value_counts()
-qtd.plot(kind='barh', figsize=(20,5), grid=True, rot=0, color='blue', legend=False)
-plt.title('Quantidade de atendimetos por mês em 2017')
-plt.xlabel('Quantidade de atendimentos')
-plt.show()
-
 %matplotlib inline
 import os
 import sys
