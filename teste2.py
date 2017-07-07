b1992fbd8ef71828fe859827d285342ca4f8baa8
ronycleyagra <ronycley@gmail.com>
2017-07-07 11:16:41 -0300

    Teste de salvamento de iamgens

---
+++ teste2.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+ts = pd.Series(np.random.randn(1000), index=pd.date_range('1/1/2000', periods=1000))
+
+ts = ts.cumsum()
+
+ts.plot()
+
+plt.savefig('/home/rony/Imagens/teste6.jpg', dpi=None, facecolor = 'w', edgecolor = 'w', orientation = 'portrait', papertype = None, format = None, transparent = False, bbox_inches = None, pad_inches = 0.1, frameon = None)
+plt.show()
+
